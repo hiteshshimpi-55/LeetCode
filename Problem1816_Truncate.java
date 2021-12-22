631e17f9ee445b3f87c49c776f571755bc96f94a
hiteshshimpi <80444596+hiteshshimpi-55@users.noreply.github.com>
2021-12-22 23:34:50 +0530

    Update Problem1816_Truncate.java

--- Problem1816_Truncate.java
+++ Problem1816_Truncate.java
@@ -13,3 +13,15 @@ class Solution {
         return ss.toString();
     }
 }
+/-100ms
+class Solution {
+    public String truncateSentence(String s, int k) {
+        int i=0,count=0;
+        for(i=0;i<s.length()&&count<k;i++){
+            if(s.charAt(i)==' '){count++;}
+            
+        }
+        if(i==s.length()){return s.substring(0,i); }
+        return s.substring(0,i-1);
+    }
+}
